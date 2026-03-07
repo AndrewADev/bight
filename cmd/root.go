889763c891641c0beb4aa8cdcf449f0544f08515
Andrew A <AndrewADev@gmail.com>
2026-03-07 11:45:40 +0100

    feat: add --version flag
    Support printing the version, including fallback to VCS info for local dev builds.

--- cmd/root.go
+++ cmd/root.go
@@ -4,8 +4,9 @@ import "github.com/spf13/cobra"
 
 func Root() *cobra.Command {
 	root := &cobra.Command{
-		Use:   "bight",
-		Short: "Patch .env files on git branch checkout",
+		Use:     "bight",
+		Short:   "Patch .env files on git branch checkout",
+		Version: resolveVersion(),
 	}
 	root.AddCommand(installCmd(), uninstallCmd(), postCheckoutCmd(), runCmd())
 	return root
